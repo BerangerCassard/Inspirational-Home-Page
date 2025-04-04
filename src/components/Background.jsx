441f34870725d12d8a7eab0044f1412655d76ac8
Béranger Cassard <beranger.cassard@gmail.com>
2025-04-04 10:14:20 +0200

    backgrouns image fetch

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,1 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+function RandomImage() {
+    const [image, setImage] = useState(null);
+    const [error, setError] = useState(null);
+
+    const fetchRandomImage = async () => {
+        try {
+            const response = await fetch('https://api.unsplash.com/photos/random', {
+                method: 'GET',
+                headers: {
+                    'Authorization': 'Client-ID 34m6xGvvYRl6Gs5jsSHZIvkoVZfGUxkZ2WyD96CsE6A',
+                },
+            });
+            const data = await response.json();
+            setImage(data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchRandomImage();
+    }, []);
+
+    return (
+        <>
+            {error && <div>Error: {error}</div>}
+            {!image ? (
+                <div>Loading...</div>
+            ) : (
+                <img 
+                    src={image.urls.regular} 
+                    alt={image.description || 'Random Image'}
+                />
+            )}
+        </>
+    );
+}
+
+export default RandomImage;
