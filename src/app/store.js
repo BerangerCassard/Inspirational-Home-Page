f65aa3f1e77a6688e45fd73adb9d8b14170d140d
Béranger Cassard <beranger.cassard@gmail.com>
2025-04-10 14:50:52 +0200

    updated the store reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import barReducer from '../features/Bar/barSlice';
-
+import cardReducer from '../features/CardTable/cardSlice';
 export const store = configureStore({
   reducer: {
     bar: barReducer,
+    card: cardReducer,
   },
 });
